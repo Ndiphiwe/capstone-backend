0cd707041ef654d3834f57398ffb79ce9bed7a0a
Ndiphiwe Ngxola <pndira1770@gmail.com>
2022-03-22 12:09:30 +0200

    make it better2

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -4,6 +4,10 @@ const controller = require("../controllers/auth.controller");
 
 const checkDuplicateUsernameOrEmail = require("../middleware/verifySignUp");
 
+router.get("/", (req, res) => {
+    res.send("Hey there... welcome to my blog API")
+});
+
 router.post("/signup", checkDuplicateUsernameOrEmail, controller.signup);
 
 router.post("/signin", controller.signin);
